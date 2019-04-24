ee751b98e07c724a713a5e33348729ce4bcc25b8
Sam <hai.luut@gmail.com>
2019-04-24 17:15:37 +0700

    Update training script to add validation step

--- main.py
+++ main.py
@@ -11,13 +11,14 @@ data_gen_args = dict(rotation_range=0.2,
                     zoom_range=0.05,
                     horizontal_flip=True,
                     fill_mode='nearest')
-myGene = trainGenerator(2,'data/ffg/train','image','label',data_gen_args,save_to_dir = None)
+myGene = trainGenerator(2,'data/hw/train','image','label',data_gen_args,save_to_dir = None)
+valGene = trainGenerator(2, 'data/hw/test', 'image', 'label', None, save_to_dir=None)
 
 model = deep_unet()
 model.summary()
-model_checkpoint = ModelCheckpoint('unet_ffg.hdf5', monitor='loss',verbose=1, save_best_only=True)
-model.fit_generator(myGene,steps_per_epoch=300,epochs=10,callbacks=[model_checkpoint])
+model_checkpoint = ModelCheckpoint('unet_hw.hdf5', monitor='loss',verbose=1, save_best_only=True)
+model.fit_generator(myGene,steps_per_epoch=300,epochs=10,callbacks=[model_checkpoint], validation_data=valGene, validation_steps=1)
 
-testGene = testGenerator("data/ffg/test")
+testGene = testGenerator("data/hw/test")
 results = model.predict_generator(testGene,10,verbose=1)
-saveResult("data/ffg/test",results)
+saveResult("data/hw/test",results)
